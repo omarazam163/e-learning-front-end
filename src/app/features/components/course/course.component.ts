364cb863554d79745459399e1d29e65155203b9a
MostafaGoda123 <104257870+MostafaGoda123@users.noreply.github.com>
2025-04-17 11:12:51 +0200

    updating courses

--- src/app/features/components/course/course.component.ts
+++ src/app/features/components/course/course.component.ts
@@ -5,57 +5,84 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { role } from '../../../shared/types/role';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-course',
-  imports: [CommonModule, RouterModule, FormsModule, RouterLink],
+  imports: [CommonModule, RouterModule, FormsModule, RouterLink ],
   templateUrl: './course.component.html',
   styleUrl: './course.component.scss',
 })
 export class CourseComponent {
   public allCourses: any;
-  public courses: any;
   public endOfCourses: number = 1;
   public inputValue: string = '';
-  public category: string = 'all';
+  public category: number = 0;
+  public role : role = "noLogin";
+  public courseURL = "https://localhost:7180/api/Courses";
+  public courses : any[] = [];
+  public categoryURL = "https://localhost:7180/api/Categories";
+  public categories : any[] = [];
+  public coursesForShow: any[] = this.courses;
 
-  constructor(public coursesService: CoursesService) {}
+  constructor(public coursesService: CoursesService , public authServices : AuthService , public http : HttpClient) {}
 
   ngOnInit(): void {
     this.allCourses = this.coursesService.courses;
     this.courses = this.allCourses.slice(0, 9 * this.endOfCourses - 1);
+    this.authServices.islogin.subscribe( (e:role) => this.role=e )
+    this.getCourses();
+    this.getCategories();
+    // console.log(this.courses);
+    // console.log(this.categories);
+    // this.coursesForShow = this.courses;
   }
 
-  handleCategory(category: string) {
+  getCourses ()  {
+    this.http.get<any>(this.courseURL).subscribe({
+      next: (res) => {
+        this.courses = res.data;
+        this.coursesForShow = this.courses;
+        // console.log(this.courses);
+      },
+      error: (err) => console.log(err)
+    })
+  }
+  getCategories() {
+    this.http.get<any>(this.categoryURL).subscribe({
+      next : (res) => {
+        this.categories = res.data;
+        this.categories.unshift({categoryId: 0, categoryName: 'All'})
+      },
+      error: (err) => console.log(err)
+    });
+  }
+
+  handleCategory(category: number) {
     this.category = category;
-    if (category == 'all') {
-      this.allCourses = this.coursesService.courses;
-    } else {
-      this.allCourses = this.coursesService.courses.filter(
-        (c) => c.category == category
-      );
+    if (category == 0) {
+      this.http.get<any>(this.courseURL).subscribe((res) => this.coursesForShow = res.data);
+    }else {
+      this.http.get<any>(`https://localhost:7180/api/Courses/${category}`).subscribe((res) => this.coursesForShow = res.data);
     }
-    this.endOfCourses = 1;
-    this.courses = this.allCourses.slice(0, 9 * this.endOfCourses - 1);
   }
 
   handleSearch() {
     this.endOfCourses = 1;
-    this.courses = this.coursesService.courses
-      .filter(
-        (c) =>
+    this.coursesForShow = this.coursesForShow.filter( (c) =>
           c.title.toLowerCase().includes(this.inputValue.toLowerCase()) ||
-          c.category.toLowerCase().includes(this.inputValue.toLowerCase()) ||
-          c.admin.toLowerCase().includes(this.inputValue.toLowerCase())
-      )
-      .slice(0, 9 * this.endOfCourses - 1);
+          c.description.toLowerCase().includes(this.inputValue.toLowerCase()) 
+          // c.instructor.toLowerCase().includes(this.inputValue.toLowerCase())
+      ).slice(0, 9 * this.endOfCourses - 1);
   }
 
   addNewCourses() {
-    if (this.allCourses.length > 9 * this.endOfCourses) {
+    if (this.coursesForShow.length > 9 * this.endOfCourses) {
       this.endOfCourses++;
-      this.courses = this.allCourses.slice(0, 9 * this.endOfCourses - 1);
+      this.coursesForShow = this.coursesForShow.slice(0, 9 * this.endOfCourses - 1);
     }
-    console.log(this.endOfCourses);
-    console.log(this.courses);
   }
 }
