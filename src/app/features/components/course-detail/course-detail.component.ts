edb00ab3bda2ecf02dfeb16ac8df055c74ce4e30
MostafaGoda123 <104257870+MostafaGoda123@users.noreply.github.com>
2025-05-03 15:32:01 +0300

    there is a wrong when posting review

--- src/app/features/components/course-detail/course-detail.component.ts
+++ src/app/features/components/course-detail/course-detail.component.ts
@@ -9,15 +9,17 @@ import { map, Observable, pipe } from 'rxjs';
 import { InstructorsService } from '../../../core/services/instructors.service';
 import { Instructor } from '../../../shared/interfaces/Instructor';
 import { Course } from '../../../shared/interfaces/course';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../../shared/interfaces/user.';
 
 @Component({
   selector: 'app-course-detail',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule , FormsModule],
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.scss',
 })
 export class CourseDetailComponent {
-  courseId: any;
+  courseId: number = 0;
   activeCourseLink: string = 'Overview';
   handleActiveLink(active: string) {
     this.activeCourseLink = active;
@@ -26,6 +28,11 @@ export class CourseDetailComponent {
   courseDetails!: Course;
   videoUrl: string = '';
   videoTitle: string = '';
+  reviewMessage : string = "";
+  reviewRating : number = 0;
+  errorReviewMessage : boolean = false;
+  errorReviewRating : boolean = false;
+  studentId : number = 0 ;
 
   constructor(
     public http: HttpClient,
@@ -37,7 +44,7 @@ export class CourseDetailComponent {
   ) {}
 
   ngOnInit(): void {
-    this.courseId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.courseId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     this.GetModules().subscribe((res) => (this.modules = res));
     this.CoursesService.getCourseById(this.courseId).subscribe(
       (res: Course) => {
@@ -60,4 +67,41 @@ export class CourseDetailComponent {
     this.videoUrl = vu;
     this.videoTitle = vt;
   }
+
+  SendReview() {
+    if (this.reviewMessage.length < 10)
+    {
+      this.errorReviewMessage = true;
+    }
+    else if (this.reviewRating == 0)
+    {
+      this.errorReviewMessage = false;
+      this.errorReviewRating = true;
+    }
+    else
+    {
+      this.errorReviewMessage = false;
+      this.errorReviewRating = false;
+      this.authService.islogin.subscribe((val) => {
+          this.authService.UserData.subscribe((user: User) => {
+            this.studentId = Number(user.Id);
+          });
+      });
+      this.http.put("https://localhost:7180/api/Reviews" , {
+        "studentId": this.studentId,
+        "courseId": this.courseId,
+        "comment": this.reviewMessage,
+        "rating": this.reviewRating
+      });
+      console.log({
+        "studentId": this.studentId,
+        "courseId": this.courseId,
+        "comment": this.reviewMessage,
+        "rating": this.reviewRating
+      });
+      this.reviewMessage = '';
+      this.reviewRating = 0;
+    }
+  }
+
 }
